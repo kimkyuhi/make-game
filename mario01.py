e71d3ead924979ec5312c98705f7a3c760c91108
2018180003김규현 <90171602+kimkyuhi@users.noreply.github.com>
2021-10-20 01:01:19 +0900

    mario can jump

--- mario01.py
+++ mario01.py
@@ -1,6 +1,8 @@
 from pico2d import *
 import threading
 
+class world_grass
+
 def thread_run():
     global charge
     global dir
@@ -22,9 +24,9 @@ def handle_running_events():
     global dir
     global charge
     global stop
-    global jump, jump_remy
-    global x, y
-
+    global jump
+    global x, y, floar
+    global jump_start, jump_middle, jump_last
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -40,7 +42,14 @@ def handle_running_events():
                 thread_run()
             elif event.key == SDLK_UP:
                 jump = 1
-                jump_remy = y
+                if dir > 0:
+                    jump_start = x, floar
+                    jump_middle = x + 20, floar + 40
+                    jump_last = x + 40, floar
+                elif dir < 0:
+                    jump_start = x, floar
+                    jump_middle = x - 20, floar + 40
+                    jump_last = x - 40, floar
             elif event.key == SDLK_ESCAPE:
                 running = False
 
@@ -54,12 +63,10 @@ def handle_running_events():
                 stop = -1
                 dir = 0
                 charge = 0
-            elif event.key == SDLK_UP:
-                jump = 0
+
     pass
 
 open_canvas()
-grass = load_image('grass.png')
 character_right = load_image('mario_right_walk.png')
 character_left = load_image('mario_left_walk.png')
 character_right_stop = load_image('mario_right_stop.png')
@@ -67,49 +74,63 @@ character_left_stop = load_image('mario_left_stop.png')
 character_right_run = load_image('mario_right_run.png')
 character_left_run = load_image('mario_left_run.png')
 character_right_jump = load_image('mario_right_jump.png')
+character_left_jump = load_image('mario_left_jump.png')
 
 running = True
 x = 0
 y = 75
+floar = 75
 walk_frame = 0
 run_frame = 0
 dir = 0 # -1 left +1 right 0 stop
 stop = 1 # -1 left +1 right 0 walk or run
 charge = 0
 jump = 0
-jump_remy = 0.0
-
+i = 0
+t = 0.0
+jump_start = x, y
+jump_middle = x, y
+jump_last = x, y
 while running:
     clear_canvas()
-    grass.draw(400, 30)
+
+
     # 정지모션
     if stop == 1:
         character_right_stop.clip_draw(0, 2, 20, 35, x, y)
     elif stop == -1:
         character_left_stop.clip_draw(0, 2, 20, 35, x, y)
     else:# walk or run and jump.
-        if dir > 0:
-            if charge == 6:
-                if jump != 1:
+        if jump != 1:
+            if dir > 0:
+                if charge == 6:
                     character_right_run.clip_draw(run_frame * 19, 2, 19, 35, x, y)
-                else:#jump
-                    while jump == 1 and y <= (jump_remy + 50):
-                        y += 3
-                    character_right_jump.clip_draw(0, 2, 20, 35, x, y)
-            else:
-                character_right.clip_draw(walk_frame * 20, 2, 20, 35, x, y)
+                else:
+                    character_right.clip_draw(walk_frame * 20, 2, 20, 35, x, y)
+            elif dir < 0:
+                if charge == -6:
+                    character_left_run.clip_draw(run_frame * 20, 2, 20, 35, x, y)
+                else:
+                    character_left.clip_draw(walk_frame * 20, 2, 20, 35, x, y)
+        else:
+            t = i / 100
+            x = (2 * t ** 2 - 3 * t + 1) * jump_start[0] + (-4 * t ** 2 + 4 * t) * jump_middle[0] + (2 * t ** 2 - t) * jump_last[0]
+            y = (2 * t ** 2 - 3 * t + 1) * jump_start[1] + (-4 * t ** 2 + 4 * t) * jump_middle[1] + (2 * t ** 2 - t) * jump_last[1]
+            if dir > 0:
+                character_right_jump.clip_draw(0, 2, 20, 35, x, y)
+            elif dir < 0:
+                character_left_jump.clip_draw(0, 2, 20, 35, x, y)
 
-        elif dir < 0:
-            if charge == -6:
-                character_left_run.clip_draw(run_frame * 20, 2, 20, 35, x, y)
-            else:
-                character_left.clip_draw(walk_frame * 20, 2, 20, 35, x, y)
+            i += 10
+            if t == 1:
+                jump = 0
+                i = 0
 
     update_canvas()
     handle_running_events()
     walk_frame = (walk_frame + 1) % 5
     run_frame = (run_frame + 1) % 3
     x += dir * 5 + charge
-    delay(0.08)
+    delay(0.05)
 
 close_canvas()
